05c148ef507d2e47ea106ce9b726d86d351b2c77
Ichijo Hiroki <hiroki.ichijo@axas-japan.co.jp>
2022-03-23 18:09:41 +0900

    Initial Commit

---
+++ Chapter6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chapter6
+//
+//  Created by AXLT0221-AP on 2022/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
